b883e17797a6e0fed5bca4abf76d0fbb1e7d7216
Munna-Scripts <munnascriptz@gmail.com>
2025-11-20 17:09:46 +0600

    Login register server created

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("hello world")
+const express = require('express')
+const app = express()
+app.use(express.json())
+
+
+
+// --------------------- Get data only 
+app.get('/' , (req, res)=>{
+    res.send("Hello world")
+})
+
+
+
+// --------------------- Post data Registration 
+app.post('/register', (req , res)=>{
+    res.send('hello world')
+})
+
+
+// --------------------- Post data Login 
+app.post('/login', (req , res)=>{
+    res.send('hello world')
+})
+
+app.listen(8000, ()=>{
+    console.log("Server is running")
+})
