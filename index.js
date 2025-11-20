43c00d8e5dbfd3c8c8db77cc0b26deee1b44f300
Munna-Scripts <munnascriptz@gmail.com>
2025-11-20 17:24:32 +0600

    Server validation complete

--- index.js
+++ index.js
@@ -1,27 +1,52 @@
-const express = require('express')
-const app = express()
-app.use(express.json())
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+// --------------------- Get data only
+app.get("/", (req, res) => {
+    res.send("Hello world");
+});
+
+// --------------------- Post data Registration
+app.post("/register", (req, res) => {
+    // ------------- Destructure
+    const { email, password, phone } = req.body;
+
+    try {
+        // ----------------- Validation
+        if (!email) return res.status(401).send("Enter your email");
+        if (!password) return res.status(401).send("Enter password");
+        if (!phone) return res.status(401).send("Enter your number");
+    } catch (error) {
+        console.log(error, "error");
+    }
+
+    res.send("Register success");
+});
+
+// --------------------- Post data Login
+app.post("/login", (req, res) => {
+    // ------------- Destructure
+    const { email, password } = req.body;
+
+    try {
+        // ----------------- Validation
+        if (!email) return res.status(401).send("Enter your email");
+        if (!password) return res.status(401).send("Enter password");
 
 
 
-// --------------------- Get data only 
-app.get('/' , (req, res)=>{
-    res.send("Hello world")
-})
+    } catch (error) {
+        console.log('Error', error)
+    }
 
 
 
-// --------------------- Post data Registration 
-app.post('/register', (req , res)=>{
-    res.send('hello world')
-})
 
+    // ---------------- Complete 
+    res.send("Login Complete");
+});
 
-// --------------------- Post data Login 
-app.post('/login', (req , res)=>{
-    res.send('hello world')
-})
-
-app.listen(8000, ()=>{
-    console.log("Server is running")
-})
+app.listen(8000, () => {
+    console.log("Server is running");
+});
