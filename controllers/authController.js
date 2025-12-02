715ba5797f524a5d40099f11ebf9816521e1aec4
Munna-Scripts <munnascriptz@gmail.com>
2025-12-02 22:58:29 +0600

    Password bycripted in a new file folder strucutre

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,15 +1,56 @@
+const userSchema = require("../models/userSchema")
+const bcrypt = require('bcrypt');
+
 // ----- register 
-const register = (req , res)=>{
-    const {username , email, password} = req.body
+const register = async (req, res) => {
+    const { username, email, password } = req.body
 
-    res.send(username)
 
+    try {
+        const hashedPass = await bcrypt.hash(password, 10);
+
+        if (!username) return res.send('username is required')
+        if (!email) return res.send('email is required')
+        if (!password) return res.send('password is required')
+        const existUser = await userSchema.findOne({ email })
+        if (existUser) return res.send('email is already taken')
+
+
+        const userDB = new userSchema({
+            username,
+            email,
+            password: hashedPass, 
+        })
+
+        userDB.save()
+
+        res.send('registration complete')
+
+    } catch (error) {
+        console.log(error)
+    }
 
 }
 
 // ------ Login 
-const login = (req , res)=>{
-    res.send('login complete')
+const login = async (req, res) => {
+    const { email, password } = req.body
+
+    try {
+        if (!email) return res.send('email is required')
+        if (!password) return res.send('password is required')
+
+        const existUser = await userSchema.findOne({ email })
+        if (!existUser) return res.send('user not found')
+
+        const match = await bcrypt.compare(password, existUser.password);
+        if(!match) return res.send("invalid password")
+
+
+        res.send('login complete')
+    } catch (error) {
+        console.log(error)
+    }
 }
 
-module.exports = {register, login}
+module.exports = { register, login }
