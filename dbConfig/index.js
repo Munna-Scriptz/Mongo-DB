65e70f74fb732d58292264bda47218939e6f6e05
Munna-Scripts <munnascriptz@gmail.com>
2025-12-03 16:23:41 +0600

    New strucutre

--- dbConfig/index.js
+++ dbConfig/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const dbConnect = ()=>{
     return mongoose.connect('mongodb+srv://Munna-Scriptz:mLuMYoUT1ZbtakeP@cluster0.1xmbxqk.mongodb.net/Test-Users?appName=Cluster0')
-    .then(() => console.log('Connected!'));
+    .then(() => console.log('DB Connected!'));
 }
 
 module.exports = dbConnect
